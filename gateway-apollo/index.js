40f484cfb393033d003b644a83305b9bcb2d4570
nodkz <pavel.chertorogov@gmail.com>
2021-05-10 22:53:08 +0600

    small fixes

--- gateway-apollo/index.js
+++ gateway-apollo/index.js
@@ -6,23 +6,12 @@ const gateway = new ApolloGateway({
     { name: 'users', url: 'http://localhost:4001' },
     { name: 'articles', url: 'http://localhost:4002' },
     { name: 'comments', url: 'http://localhost:4003' },
-    // more services
   ],
   buildService: (service) => {
     return new RemoteGraphQLDataSource({
       url: service.url,
       willSendRequest: ({ request }) => {
-        console.log(request.query);
-      },
-      didReceiveResponse: (o) => {
-        if (o.request.query === 'query __ApolloGetServiceDefinition__ { _service { sdl } }') {
-          o.response.http.status = 200;
-          o.response.data = {
-            _service: {
-              sdl: '',
-            },
-          };
-        }
+        // console.log(request.query);
       },
     });
   },
